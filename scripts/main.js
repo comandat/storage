a03cf197a8a8ac925ce27ffa0bd74f3ae2f625db
comandat <contact@comandat.ro>
2025-11-03 20:53:49 +0200

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+// --- Inițializare ---
+document.addEventListener("DOMContentLoaded", () => {
+    html5QrCode = new Html5Qrcode("reader");
+    loadInitialStorage(); // Încarcă stocul și apoi preia comenzile
+    showPage('page-dashboard');
+
+    // Listener pentru a închide căutarea dacă se dă click în afara ei
+    document.getElementById('app-container').addEventListener('click', (e) => {
+        const searchForm = document.getElementById('search-form');
+        if (!searchForm.contains(e.target) && !document.getElementById('find-results').contains(e.target)) {
+            toggleSearchFocus(false);
+        }
+    });
+    
+    // Event Listeners pentru Căutare
+    const searchInput = document.getElementById('search-input');
+    if (searchInput) {
+        searchInput.addEventListener('input', searchProducts);
+        searchInput.addEventListener('focus', () => toggleSearchFocus(true));
+    }
+    
+    // Setează starea inițială a footer-ului
+    setupPickingPageFooter(false);
+});
